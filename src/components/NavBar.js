3283802a74f2c4f6a8b8d8fae9bbda96ad838e00
satishkarki <satish.karki94@gmail.com>
2021-10-06 10:53:32 -0400

    Add NavBar Status

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {navlinks} from "./NavItem";
+import "../style/NavBar.css"
+import {FaUserNinja} from "react-icons/fa";
 
 function NavBar(){
     const NavMenu=navlinks.map((navlink)=>
-    <li key={navlink.id}>
-        <Link to={navlink.path}>
-        {navlink.icon}
-        <span>{navlink.name}</span>
-        </Link>
-    </li>
+        
+        
+            <li className="nav-item" key={navlink.id}>
+            <Link to={navlink.path} >
+                {navlink.icon}
+                <span >{navlink.name}</span>
+            </Link>
+            </li>
+            
+    
+  
+   
     );
+
+    const BrandName=(
+        <div className="BrandDiv">
+            <Link to="/" className="navlogo">
+                <FaUserNinja/>
+                <span className="logo-name">SATISH KARKI</span>
+            </Link>
+        </div>
+    
+      
+    );
+ 
     return(
-        <div>
+        <div className="Header">
+            {BrandName}
+            <ul className="nav-items">
             {NavMenu}
+            </ul>
         </div>
     );
 }
