41556921c12d907258b6822123a550386a72c61c
satishkarki <satish.karki94@gmail.com>
2021-10-10 01:13:12 -0400

    Step 1 Education Component Add

---
+++ src/components/TimelineElements.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaMapMarkerAlt,FaRegCalendarAlt,FaUniversity,FaBookReader} from "react-icons/fa";
+export const EducationLinks=[
+    {
+        id:1,
+        college:"Georgian College",
+        collegeIcon:<FaUniversity/>,
+        program:"Information System Security",
+        programIcon:<FaBookReader/>,
+        year:"2020-2021",
+        yearIcon:<FaRegCalendarAlt/>,
+        location:"Barrie, ON, Canada",
+        locationIcon:<FaMapMarkerAlt/>
+
+    },
+    {
+        id:2,
+        college:"Georgian College",
+        collegeIcon:<FaUniversity/>,
+        program:"Big Data Analytics",
+        programIcon:<FaBookReader/>,
+        year:"2019-2020",
+        yearIcon:<FaRegCalendarAlt/>,
+        location:"Barrie, ON, Canada",
+        locationIcon:<FaMapMarkerAlt/>
+    },
+    {
+        id:3,
+        college:"Kathmandu University",
+        collegeIcon:<FaUniversity/>,
+        program:"BE Electrical Engineering",
+        programIcon:<FaBookReader/>,
+        year:"2013-2017",
+        yearIcon:<FaRegCalendarAlt/>,
+        location:"Kathmandu, Nepal",
+        locationIcon:<FaMapMarkerAlt/>
+    }
+    
+];
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,58 @@
 import React from "react";
+import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css';
+
 import "../style/About.css"
+import {EducationLinks} from "../components/TimelineElements"
+import {FaUserGraduate, FaTools,FaWarehouse,} from "react-icons/fa";
 
 function About() {
+
+
+  function Education(){
+    let darkbackground={background:"#1b1919", color: "whitesmoke"}
+    return(
+      <div>
+        <VerticalTimeline>
+          {
+            EducationLinks.map(educationlink=>{
+              return(
+               <VerticalTimelineElement
+                key={educationlink.id}
+                date={educationlink.year}
+                dateClassName="date"
+                iconStyle={darkbackground}
+                icon={<FaUserGraduate/>}
+                >
+                <h4 className="vertical-timeline-element-title">{educationlink.program}</h4>
+                <h5 className="vertical-timeline-element-subtitle">{educationlink.collegeIcon} {educationlink.college}</h5>
+                <h6 className="vertical-timeline-element-subtitle">{educationlink.locationIcon} {educationlink.location}</h6>
+
+                </VerticalTimelineElement>
+
+              
+             
+               
+            )     
+          })
+        }
+      </VerticalTimeline>
+    </div>
+  );
+  }
+
+
+  
+ 
+  
+
   return (
    <section className="about">
     <div class="title">
       <h2>About Me</h2>
     </div>
+    <h1 className="heading">Education</h1>
+    <Education/>
    </section>
   );
 }
